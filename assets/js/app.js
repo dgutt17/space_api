e65845b1b0cc5c1ca9c3a0b0ae37e9c6e33c51df
Dan Gutt <dgutt10@gmail.com>
2022-02-24 21:59:47 -0600

    Finally got JS to work in a more modular way

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,6 +25,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import preload from "../vendor/game/preload.js"
 import game from "../vendor/game"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
